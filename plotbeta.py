0f49bd854878f4469e749c73d7c24d1c6da6b3db
hehaijin <hehaijin1981@gmail.com>
2017-10-18 14:43:20 -0600

    report questions 4 and 5

--- plotbeta.py
+++ plotbeta.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 
 beta=[0.00001,0.0001,0.001,0.01,0.1,1]
-result=[5,4,3,2,1,0]
+result=[0.87038,0.87688,0.88249,0.88692,0.88396,0.85090]
 
 plt.semilogx(beta, result)
-plt.title('beta and accuracy')
+plt.title('beta and accuracy relation')
+plt.xlabel('beta')
+plt.ylabel('accuracy')
 plt.grid(True)
 plt.show()
  
